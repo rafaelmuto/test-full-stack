1320e4f78223a01e63373e962ccb00340354d159
Rafael Muto <r.nagahama@gmail.com>
2018-11-06 18:46:37 -0200

    commit inicial da branch

---
+++ main.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Teste de Recrutamento Full Stack PHP</title>
+  </head>
+  <body>
+    <h1>test e initial commit</h1>
+  </body>
+</html>

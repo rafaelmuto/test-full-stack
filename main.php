ba4464466f51bd2cd184aa5341c7cc0683fbe67b
Rafael Muto <r.nagahama@gmail.com>
2018-11-07 11:10:51 -0200

    frontend teste

--- main.php
+++ main.php
@@ -2,9 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,400" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
     <title>Teste de Recrutamento Full Stack PHP</title>
   </head>
   <body>
-    <h1>test e initial commit</h1>
+    <header>
+      <h1>crudDB</h1>
+    </header>
   </body>
 </html>

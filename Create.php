afcec9e3ab72a870a8b3b868bd8af24fd754f651
Rafael Muto <r.nagahama@gmail.com>
2018-11-07 11:44:16 -0200

    ajustes no frontend, implementado grids

--- Create.php
+++ Create.php
@@ -13,30 +13,36 @@
       <h1>CRUD - Create</h1>
     </header>
 
-    <div class="form_usuario">
-      <form action="switch.php" method="post">
-        Nome: <input type="text" name="nome" value="">
-        E-mail: <input type="text" name="email" value="">
-        Senha: <input type="password" name="senha" value="">
-        Conf. Senha: <input type="password" name="conf_senha" value="">
-        <button type="submit" name="action" value="cadastro">Cadastrar!</button>
-      </form>
-    </div>
-    <hr>
-    <div class="form_catProduto">
-      <form action="switch.php" method="post">
-        Nova Categoria de Produto: <input type="text" name="cat_nome" value="">
-        <button type="submit" name="action" value="cad_catProduto">Cadastrar!</button>
-      </form>
-    </div>
-    <hr>
-    <div class="form_catProduto">
-      <form action="switch.php" method="post">
-        Nova Categoria de Publicacao: <input type="text" name="cat_nome" value="">
-        <button type="submit" name="action" value="cad_catPublicacao">Cadastrar!</button>
-      </form>
-    </div>
+    <div class="main_container">
 
+      <div class="form_usuario">
+        <h2>Usuarios</h2>
+        <form action="switch.php" method="post">
+          Nome: <input type="text" name="nome" value="">
+          E-mail: <input type="text" name="email" value="">
+          Senha: <input type="password" name="senha" value="">
+          Conf. Senha: <input type="password" name="conf_senha" value="">
+          <button type="submit" name="action" value="cadastro">Cadastrar!</button>
+        </form>
+      </div>
+
+      <div class="form_catProduto">
+        <h2>Categoria de Produtos</h2>
+        <form action="switch.php" method="post">
+          Nova Categoria de Produto: <input type="text" name="cat_nome" value="">
+          <button type="submit" name="action" value="cad_catProduto">Cadastrar!</button>
+        </form>
+      </div>
+
+      <div class="form_catPublicacao">
+        <h2>Categoria de Publicacao</h2>
+        <form action="switch.php" method="post">
+          Nova Categoria de Publicacao: <input type="text" name="cat_nome" value="">
+          <button type="submit" name="action" value="cad_catPublicacao">Cadastrar!</button>
+        </form>
+      </div>
+
+    </div>
 
   </body>
 </html>
